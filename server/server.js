e892c1f81109b011658abee091ccad41282dce5a
Max Kahn <kahn.max@gmail.com>
2016-01-27 10:53:02 -0800

    mp3 files with static and beeps

--- server/server.js
+++ server/server.js
@@ -7,6 +7,9 @@ var app = express();
 var binaryServer = require('binaryjs').BinaryServer;
 var wav = require('wav');
 var opener = require('opener');
+var rm = require('rimraf');
+var ffmpeg = require('fluent-ffmpeg');
+var binary = require('binary');
 
 var lame = require('lame');
 
@@ -59,6 +62,14 @@ server.on('connection', function(client) {
 
     console.log("Stream Start@" + meta.sampleRate + "Hz");
 
+    //I believe the stream is a binary stream
+    var binaryStream = binary(stream);
+
+    //all I want to do for now is *test* the stream
+      //to check its endianness how do I do that?
+
+    //***VERSION WITH WAV***
+
     // var fileName = "recordings/" + new Date().getTime() + ".wav"
 
     // fileWriter = new wav.FileWriter(fileName, {
@@ -76,90 +87,106 @@ server.on('connection', function(client) {
     //   console.log("Connection Closed");
     // });
       console.log(meta);
-      // stream.on('data', function(data) {
-      //push data to all connected clients
-      //todo fix delay
       console.log('on data');
-
-      //so in theory, instead of writing to a file, I could write to an output stream
       var myStream = fs.createWriteStream('outputFromStream.mp3');
 
       var encoder = new lame.Encoder({
+        //sample rate = frequency, bitrate = volume
+        //input before I started comparing with wav input
+        // channels: 2,
+        // bitDepth: 32,
+        // float: true,
+        // sampleRate: 44100,
         channels: 2,
-        bitDepth: 32,
-        float: true,
-        sampleRate: 44100,
-
+        bitDepth: 16, 
+        // float: true,
         bitRate: 128,
-        outSampleRate: 22050,
-        mode: lame.MONO
+        endianness: 'big',
+        outSampleRate: 44100,
+        // mode: lame.STEREO
       });
 
-      stream.on('data', function(data) {
-        stream.pipe(encoder);
-        encoder.pipe(myStream);
-      })
-    //   stream.pipe(new lame.Encoder({
-    //   // input 
-    //   channels: 2, // 2 channels (left and right) 
-    //   bitDepth: 32, // 16-bit samples 
-    //   float: true,
-    //     sampleRate: 44100, // 44,100 Hz sample rate 
 
-    //   // // output 
-    //   bitRate: 128,
-    //   outSampleRate: 22050,
-    //   mode: lame.MONO // STEREO (default), JOINTSTEREO, DUALCHANNEL or MONO 
-    // }));
+      //wondering if I even need the data cb?
+      //this line should just work
+        // stream.pipe(encoder);
+        // encoder.pipe(myStream);
+
+    //new attempt to nest stream, encoder, and data
+      stream.on('data', function(data) {
+        encoder.write(data, function() {
+          encoder.on('data', function(data) {
+            myStream.write(data);
+          });
+        });
+      });
 
       stream.on('end', function() {
         console.log('done');
+        myStream.end();
       });
       stream.on('close', function() {
         console.error('done!');
+        myStream.end();
       });
     });
 
-
-    // var encoder = new lame.Encoder({
-    //   // input 
-    //   channels: 2, // 2 channels (left and right) 
-    //   bitDepth: 32, // 16-bit samples 
-    //     sampleRate: 44100, // 44,100 Hz sample rate 
-
-    //   // // output 
-    //   bitRate: 128,
-    //   outSampleRate: 22050,
-    //   mode: lame.STEREO // STEREO (default), JOINTSTEREO, DUALCHANNEL or MONO 
-    // });
-
-    // encoder.on("data", function(data) {
-    //   console.log('encoder');
-    //   sendData(data);
-    // });
-
-    // var decoder = new lame.Decoder();
-
-    // decoder.on('format', function(format) {
-    //   console.log('decoder');
-    //   decoder.pipe(encoder);
-    // });
-
-    // strea÷m.pipe(encoder);
-
-    // stream.on('data', function(data) {
-    //   //push data to all connected clients
-    //   //todo fix delay
-    //   console.log('on data');
-    //   encoder.write(data);
-    // });
-
-    // var sendData = function(data) {
-    //   console.log('send data to client before');
-    //   clients.forEach(function(client) {
-    //     console.log('send data to client');
-    //     client.write(data);
-    //   });
-    // }
-
   });
+
+
+//from mido22's recordOpus app
+  //what kind of objects are inFile and outFile
+var audioConcat = function(inFile, outFile, callback) {
+  try {
+    ffmpeg().input(inFile)
+      .inputOptions('-f', 'concat')
+      .output(outFile)
+      .on('error', function(err) {
+        console.log('err: ', err);
+        callback(err);
+      })
+      .on('end', function() {
+        callback(null, outFile);
+      })
+      .run();
+  }
+
+  catch(e) {
+    console.log('err: ', e);
+    callback(e);
+  }
+};
+
+//from mido22's recordOpus app
+  //data is a piece of audio buffer
+  //assumes client-side browser !== FireFox
+var saveAudio = function(data, callback) {
+  //mido22 has a first call to appendFile
+    //I think that's just for the file name, and I'll ignore that
+  data.path = 'temp' + data.uid;
+  fs.appendFile('outputFromStream.mp3', data.blob, function(err) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    if (data.stop) {
+      //I may have confused these two files
+      var outFile = 'outputFromStream.mp3';
+      var inFile = data.path;
+      concat(inFile, outFile, function(err, filepath) {
+        rm(data.path, function(err) {
+          if (err) {
+            console.log('err: ', err);
+          }
+        });
+        returnLink(data, filepath, callback);
+      })
+    }
+  })
+};
+
+var returnLink = function(data, filepath, callback) {
+  data.path = filepath;
+  delete data.blob;
+  callback(data);
+};
